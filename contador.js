c3c14792988beafe1f136381f1065a556ac9ff37
Rafael PacMe <rafy.pachecomendez@gmail.com>
2016-08-21 23:44:14 -0300

    add all js scripts

---
+++ contador.js
@@ -0,0 +1,12 @@
+$(".contador").click(function (e){
+
+
+    var count = 0;
+    for(var i = 0; i < array.length; ++i){
+        if(array[i] == 10)
+        {this}
+        else{count++};
+    }
+
+    $(this).pretend("<a href='easter.html'></a>");
+});
